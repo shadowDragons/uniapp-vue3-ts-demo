5946c21209526731852bf8d403c343061699c1d2
kakalota <shadow_dragon@126.com>
2024-02-27 17:07:50 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import CustomNavBar from './componets/CustomNavbar.vue'
+import { getHomeBannerAPI } from '@/services/home'
+import type { BannerItem } from '@/types/home'
+import { onLoad } from '@dcloudio/uni-app'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <CustomNavBar />
-  <XtxSwiper />
-  <view class="index">index1234</view>
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style lang="scss">
